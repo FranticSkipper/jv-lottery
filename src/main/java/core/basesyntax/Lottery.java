21d308b598794afe32759872efc5281996c33b31
Oleg <oleg@gmail.com>
2025-08-15 15:25:42 +0300

    implement lottery ball generation logic

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,17 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    private static final int MAX_BALL_NUMBER = 100;
+
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+
+    public Ball getRandomBall() {
+        return new Ball(
+            this.colorSupplier.getRandomColor(),
+            this.random.nextInt(MAX_BALL_NUMBER + 1)
+        );
+    }
+}
